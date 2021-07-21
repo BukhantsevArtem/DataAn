f74ce037777b363e19b7c8dcb6302ae282e57d4b
Artem Bukhantsev <bukhantsev.artem@outlook.com>
2021-07-21 12:30:28 +0300

    Last lines of code

--- capsproject.py
+++ capsproject.py
@@ -2,10 +2,14 @@ import numpy as np
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
+from sklearn import preprocessing
 
 
+#Import data
 fandango = pd.read_csv("fandango_scrape.csv")
 all_sites = pd.read_csv('all_sites_scores.csv')
+
+
 plt.figure(figsize=(10,4), dpi = 200)
 sns.scatterplot(data = fandango, x = 'RATING', y = 'VOTES')
 fandango.corr()
@@ -49,12 +53,70 @@ all_sites['DIFF_ROTT'] = all_sites['RottenTomatoes'] - all_sites['RottenTomatoes
 
 np.mean(np.absolute(all_sites['DIFF_ROTT']))
 
+#Hist for RT 
 plt.figure(figsize=(10,5), dpi = 200)
 sns.histplot(data = all_sites, x = 'DIFF_ROTT', bins = 20, kde = True)
 plt.title('RT critics score minus RT User score')
 
+#Hist for RT 
 plt.figure(figsize=(10,5), dpi = 200)
 sns.histplot(x = np.absolute(all_sites['DIFF_ROTT']), bins = 20, kde = True)
 plt.title('ABS difference between critics score and RT User score')
 
-all_sites.nsmallest(5, 'DIFF_ROTT')
+#The biggest difference between users and critics
+all_sites.nsmallest(5, 'DIFF_ROTT')[['FILM', 'DIFF_ROTT']]
+all_sites.nlargest(5, 'DIFF_ROTT')[['FILM', 'DIFF_ROTT']]
+
+#Scatterplot for metac
+plt.figure(figsize=(10,5), dpi = 200)
+sns.scatterplot(data = all_sites, x = 'Metacritic', y='Metacritic_User')
+
+#Scatter plot imdb vs metac
+plt.figure(figsize=(10,5), dpi = 200)
+sns.scatterplot(data = all_sites, x = 'Metacritic_user_vote_count', y = 'IMDB_user_vote_count')
+
+all_sites.nlargest(1,'IMDB_user_vote_count')
+all_sites.nlargest(1,'Metacritic_user_vote_count')
+
+#Join data
+all_data = pd.merge(fandango, all_sites, how = 'inner', on='FILM')
+all_data.info()
+
+#Norm scores
+all_data['RT_Norm'] = np.round(all_data['RottenTomatoes']/20,1)
+all_data['RTU_Norm'] =  np.round(all_data['RottenTomatoes_User']/20,1)
+all_data['Meta_Norm'] =  np.round(all_data['Metacritic']/20,1)
+all_data['Meta_U_Norm'] =  np.round(all_data['Metacritic_User']/2,1)
+all_data['IMDB_Norm'] = np.round(all_data['IMDB']/2,1)
+
+norm_scores = all_data[['STARS','RATING','RT_Norm','RTU_Norm','Meta_Norm','Meta_U_Norm','IMDB_Norm']]
+
+#KDE plot ratings
+
+def move_legend(ax, new_loc, **kws):
+    old_legend = ax.legend_
+    handles = old_legend.legendHandles
+    labels = [t.get_text() for t in old_legend.get_texts()]
+    title = old_legend.get_title().get_text()
+    ax.legend(handles, labels, loc=new_loc, title=title, **kws)
+    
+    
+fig, ax = plt.subplots(figsize=(15,6),dpi=150)
+sns.kdeplot(data=norm_scores,clip=[0,5],shade=True,palette='Set1',ax=ax)
+move_legend(ax, "upper left")
+
+fig, ax = plt.subplots(figsize=(15,6),dpi=150)
+sns.kdeplot(data=norm_scores[['RT_Norm', 'STARS']],clip=[0,5],shade=True,palette='Set1',ax=ax)
+move_legend(ax, "upper left")
+
+fig, ax = plt.subplots(figsize=(15,6),dpi=150)
+sns.histplot(data = norm_scores, palette='Set1',ax=ax, bins = 50)
+move_legend(ax, "upper left")
+
+sns.clustermap(data = norm_scores, lw = 0.5, annot = True, cmap="plasma", col_cluster=False)
+
+fig, ax = plt.subplots(figsize=(15,6),dpi=150)
+sns.kdeplot(data=all_data.nsmallest(10, 'RT_Norm')[['STARS','RATING','RT_Norm','RTU_Norm','Meta_Norm','Meta_U_Norm','IMDB_Norm']],clip=[0,5],shade=True,palette='Set1',ax=ax)
+plt.title("Ratings for RT Critic's 10 Worst Reviewed Films");
+
+all_data[['STARS','RATING','RT_Norm','RTU_Norm','Meta_Norm','Meta_U_Norm','IMDB_Norm']].iloc[21]
